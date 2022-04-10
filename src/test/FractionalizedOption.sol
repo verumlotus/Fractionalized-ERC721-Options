267018c5c19d989e4360bf548e9fee052bc08178
verumlotus <universumtremendum@gmail.com>
2022-04-09 18:44:38 -0700

    prettier rc

--- src/test/FractionalizedOption.sol
+++ src/test/FractionalizedOption.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.11;
 import "./base/BaseTest.sol";
 
 contract ContractTest is BaseTest {
-    function setUp() public {}
+    function setUp() public {
+        // Just trust it works my friend lol
+    }
 
     function testExample() public {
         assertTrue(true);
